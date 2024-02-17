4540640c235e0e88185e0e0aef401dc02e19b0fd
mortergun <J-NoCopyright@outlook.com>
2024-02-17 13:47:22 -0600

    empty project

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import Express from "express";
+import cors from "cors";
+import db from "./utils/database";
+import initModels from "./models/initModels";
+import "dotenv/config";
+
+initModels();
+
+db.authenticate()
+  .then(() => console.log("Base de datos conectada correctamente"))
+  .catch((e) => console.error(e))
+
+db.sync() 
+  .then(() => console.log("Base de datos sincronizada"))
+  .catch((e) => console.error(e))
+
+const PORT = process.env.PORT ?? 8000;
+const app = Express();
+
+app.use(Express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Ok");
+});
+
+app.listen((PORT), () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
---
+++ src/models/initModels.js
@@ -0,0 +1,5 @@
+const initModels = () => {
+  // inicializamos las tablas de la base de datos a partir del modelo que se encuentra en models/index.
+};
+
+export default initModels;
